7b217ff1387373816943f9e6e366caa7789831fd
Aayush <106085120+AyushSharma64047@users.noreply.github.com>
2022-09-11 15:16:41 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,24 +1,24 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
-USERBOT_STRING_SESSION = ''
+API_ID = 9323694
+API_HASH = '34a0b2551aacd866c3729f7044525353'
+BOT_TOKEN = '5527478253:AAH5Bmmnv9hNpGQASZMEZZj-igdUyC95RaA'
+USERBOT_STRING_SESSION = 'BQAwneNl2snAFhT8g_1ZJUl3h-jjrWrpzSFqR1fNgJ98Q9SdGzkMfRPYrYK_t2TzwOJRyCYVEjrRdXW1YgSr7jFIQfAHzGSLzXgftUJq_w1Lyh-E_XN03i5PgS1jHKyxcMV8zwIEMnC8HukF4pjCeQKJSSujHI3Qp3Kyd6P38MCNCzuFjkSfVBzUgfQm2UJcUbSPphWw0Xt--T74ZymCMVjab7yU6goAjqlOA5j3VxJnmj3IiEgb1pxioxfBOFl8nzaDHx37lfMMpPGOrZ9fCldnFmAaa4Z7JkmfVZX6B0a46IOyx35QN9yhtaWuDHXkT0vQZ-ICYNdf6yjc-h4fPF77AAAAATp6iEcA'
 
 # Bot settings
-CACHE_TIME = 300
+CACHE_TIME = 30
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
-AUTH_USERS = []
-AUTH_CHANNEL = None
+ADMINS = [5276076103,1413071149]
+CHANNELS = [-1001557431626, 'z_harbour']
+AUTH_USERS = False
+AUTH_CHANNEL = [-1001557431626]
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'Telegram'
-COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+DATABASE_URI = "mongodb+srv://Ayush:Ayush@cluster1.bx09sol.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'ZHarbour'
+COLLECTION_NAME = 'Telegram_files'  # If you are using the same database, then use different collection name for each bot
 
 
